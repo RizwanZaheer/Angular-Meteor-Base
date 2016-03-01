4bb19facc02c2e48e3cc7ec91c7285fa9d3437fe
Mustafa Hossaini <mustafahossaini@gmail.com>
2016-03-01 14:59:44 +1300

    Revert "Revert "fast click""
    This reverts commit 088d5a0917a688b317d55f08b8a88b4dc15bd822.

--- client/app.js
+++ client/app.js
@@ -12,6 +12,15 @@ App.config([
   '$mdIconProvider',
   function($mdIconProvider, $locationProvider, $stateProvider, $urlRouterProvider,$mdIconProvider) {
 
+    $mdIconProvider.iconSet("alert", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-alert.svg");
+    $mdIconProvider.iconSet("av", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-av.svg");
+    $mdIconProvider.iconSet("device", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-device.svg");
+    $mdIconProvider.iconSet("editor", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-editor.svg");
+    $mdIconProvider.iconSet("file", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-file.svg");
+    $mdIconProvider.iconSet("hardware", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-hardware.svg")
+    $mdIconProvider.iconSet("maps", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-maps.svg")
+    $mdIconProvider.iconSet("notification", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-notification.svg")
+    $mdIconProvider.iconSet("hardware", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-hardware.svg")
     $mdIconProvider.iconSet("social", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-social.svg");
     $mdIconProvider.iconSet("action", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-action.svg");
     $mdIconProvider.iconSet("communication", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-communication.svg");
@@ -20,7 +29,7 @@ App.config([
     $mdIconProvider.iconSet("navigation", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-navigation.svg");
     $mdIconProvider.iconSet("image", "/packages/planettraining_material-design-icons/bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-image.svg")
 
-    // ui router
+      // ui router
     $locationProvider.html5Mode(false);
     $urlRouterProvider.otherwise("/");
 
